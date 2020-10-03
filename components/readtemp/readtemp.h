4f2a9324c30e9808201a4841518b88cd14a8aa5c
Dan B <dbeauchesne@gmail.com>
2020-10-03 08:38:19 -0700

    added components

---
+++ components/readtemp/readtemp.h
@@ -0,0 +1,15 @@
+//
+// Created by dan on 2020-05-25.
+//
+
+#ifndef READTEMP_H
+#define READTEMP_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+float read_temperature();
+#endif //TEMPTRACK_TEMP_H
+
