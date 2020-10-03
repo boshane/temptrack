70093b9e4b8e31026c62fbb1604e431b1e6e7a78
Dan B <dbeauchesne@gmail.com>
2020-10-03 08:37:09 -0700

    first

---
+++ main/temp.h
@@ -0,0 +1,15 @@
+//
+// Created by dan on 2020-05-25.
+//
+
+#ifndef TEMPTRACK_TEMP_H
+#define TEMPTRACK_TEMP_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+float read_temperature();
+#endif //TEMPTRACK_TEMP_H
+
